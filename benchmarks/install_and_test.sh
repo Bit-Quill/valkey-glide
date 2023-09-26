f84e3450c8de19dbc4fd32347b5d2aa2703caff9
acarbonetto <andrewc@bitquilltech.com>
2023-09-25 22:47:29 -0700

    Add Readme and update install_and_test script to runJava
    Signed-off-by: acarbonetto <andrewc@bitquilltech.com>

--- benchmarks/install_and_test.sh
+++ benchmarks/install_and_test.sh
@@ -78,6 +78,14 @@ function runJavaBenchmark(){
   cd ${BENCH_FOLDER}/java
 }
 
+function runJavaBenchmark(){
+  cd ${BENCH_FOLDER}/../java
+  echo "./gradlew run --args=\"--resultsFile=${BENCH_FOLDER}/$1 --clients $chosenClients --host $host --port $port\""
+#  ./gradlew run --args="--resultsFile=../$1 --dataSize $2 --concurrentTasks $concurrentTasks --clients $chosenClients --host $host --port $port --clientCount $clientCount $tlsFlag"
+  ./gradlew run --args="--resultsFile=${BENCH_FOLDER}/$1 --clients $chosenClients --host $host --port $port"
+  cd ${BENCH_FOLDER}/java
+}
+
 function runRustBenchmark(){
   rustConcurrentTasks=
   for value in $concurrentTasks
@@ -279,8 +287,6 @@ do
         runRustBenchmark $rustResults $currentDataSize
     fi
 done
-
-
 
 flushDB
 
