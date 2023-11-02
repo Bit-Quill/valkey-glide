e3f7596a0ddc80834c93d6556467b20c26aab088
Jonathan Louie <jonathanl@bitquilltech.com>
2023-11-02 11:37:27 -0700

    Change number of threads in Benchmarking threadpool

--- java/benchmarks/src/main/java/javababushka/benchmarks/utils/Benchmarking.java
+++ java/benchmarks/src/main/java/javababushka/benchmarks/utils/Benchmarking.java
@@ -220,7 +220,7 @@ public class Benchmarking {
                 concurrentNum, clientCount, tasks.size());
           }
           long before = System.nanoTime();
-          ExecutorService threadPool = Executors.newFixedThreadPool(concurrentNum);
+          ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
           // create threads and add them to the async pool.
           // This will start execution of all the concurrent tasks.
