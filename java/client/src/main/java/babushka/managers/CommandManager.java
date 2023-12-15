df19021ed1194c967acfe0e0d27d699ab3aadd6e
Yury-Fridlyand <yury.fridlyand@improving.com>
2023-12-15 15:52:25 -0800

    Managers
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- java/client/src/main/java/babushka/managers/CommandManager.java
+++ java/client/src/main/java/babushka/managers/CommandManager.java
@@ -1,23 +1,88 @@
 package babushka.managers;
 
-import babushka.api.commands.Command;
+import babushka.connectors.handlers.ChannelHandler;
+import babushka.ffi.resolvers.RedisValueResolver;
+import babushka.models.RequestBuilder;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
+import lombok.RequiredArgsConstructor;
+import redis_request.RedisRequestOuterClass.RequestType;
 import response.ResponseOuterClass.Response;
 
+@RequiredArgsConstructor
 public class CommandManager {
 
+  /** UDS connection representation. */
+  private final ChannelHandler channel;
+
   /**
-   * @param command
-   * @param responseHandler
-   * @return
+   * Client state, which {@link CommandManager} can flick to closed if corresponding error received.
    */
-  public <T> CompletableFuture<T> submitNewCommand(
-      Command command, Function<Response, T> responseHandler) {
-    // register callback
-    // create protobuf message from command
-    // submit async call
-    // handle return type in the thenApplyAsync
-    return new CompletableFuture<>();
+  private final ClientState.ClosableClientState clientState;
+
+  /**
+   * Async (non-blocking) get.<br>
+   * See <a href="https://redis.io/commands/get/">REDIS docs for GET</a>.
+   *
+   * @param key The key name
+   */
+  public CompletableFuture<String> get(String key) {
+    return submitNewCommand(RequestType.GetString, List.of(key));
+  }
+
+  /**
+   * Async (non-blocking) set.<br>
+   * See <a href="https://redis.io/commands/set/">REDIS docs for SET</a>.
+   *
+   * @param key The key name
+   * @param value The value to set
+   */
+  public CompletableFuture<String> set(String key, String value) {
+    return submitNewCommand(RequestType.SetString, List.of(key, value));
+  }
+
+  /**
+   * Build a command and submit it Netty to send.
+   *
+   * @param command Command type
+   * @param args Command arguments
+   * @return A result promise
+   */
+  private CompletableFuture<String> submitNewCommand(RequestType command, List<String> args) {
+    if (!clientState.isConnected()) {
+      throw new IllegalStateException("Connection is not open");
+    }
+
+    // TODO this explicitly uses ForkJoin thread pool. May be we should use another one.
+    return CompletableFuture.supplyAsync(
+            () -> channel.write(RequestBuilder.prepareRedisRequest(command, args), true))
+        // TODO: is there a better way to execute this?
+        .thenComposeAsync(f -> f)
+        .thenApplyAsync(this::extractValueFromResponse);
+  }
+
+  /**
+   * Check response and extract data from it.
+   *
+   * @param response A response received from Babushka
+   * @return A String from the Redis RESP2 response, or Ok. Otherwise, returns null
+   */
+  private String extractValueFromResponse(Response response) {
+    if (response.hasRequestError()) {
+      // TODO do we need to support different types of exceptions and distinguish them by type?
+      throw new RuntimeException(
+          String.format(
+              "%s: %s",
+              response.getRequestError().getType(), response.getRequestError().getMessage()));
+    } else if (response.hasClosingError()) {
+      CompletableFuture.runAsync(channel::close);
+      clientState.disconnect();
+      throw new RuntimeException("Connection closed: " + response.getClosingError());
+    } else if (response.hasConstantResponse()) {
+      return response.getConstantResponse().toString();
+    } else if (response.hasRespPointer()) {
+      return RedisValueResolver.valueFromPointer(response.getRespPointer()).toString();
+    }
+    return null;
   }
 }
