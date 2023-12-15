b1c4c5c571d67bb763c039f12fcf4a55634d89d1
Yury-Fridlyand <yury.fridlyand@improving.com>
2023-12-15 15:52:25 -0800

    Managers
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- java/client/src/main/java/glide/ffi/resolvers/GlideCoreNativeDefinitions.java
+++ java/client/src/main/java/glide/ffi/resolvers/GlideCoreNativeDefinitions.java
@@ -1,9 +1,14 @@
 package glide.ffi.resolvers;
 
+<<<<<<<< HEAD:java/client/src/main/java/glide/ffi/resolvers/GlideCoreNativeDefinitions.java
 public class GlideCoreNativeDefinitions {
   public static native String startSocketListenerExternal() throws Exception;
+========
+public class SocketListenerResolver {
+>>>>>>>> 337bcd7 (Managers):java/client/src/main/java/glide/ffi/resolvers/SocketListenerResolver.java
 
-  public static native Object valueFromPointer(long pointer);
+  /** Make an FFI call to Babushka to open a UDS socket to connect to. */
+  private static native String startSocketListener() throws Exception;
 
   static {
     System.loadLibrary("glide-rs");
@@ -16,7 +21,7 @@ public class GlideCoreNativeDefinitions {
    */
   public static String getSocket() {
     try {
-      return startSocketListenerExternal();
+      return startSocketListener();
     } catch (Exception | UnsatisfiedLinkError e) {
       System.err.printf("Failed to create a UDS connection: %s%n%n", e);
       throw new RuntimeException(e);
