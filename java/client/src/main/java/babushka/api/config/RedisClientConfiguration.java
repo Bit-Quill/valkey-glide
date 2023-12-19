35997f398e77133ceda42780cf1439eaf286d406
SanHalacogluImproving <san.halacoglu@improving.com>
2023-12-19 15:15:42 -0800

    Changed Access Modifiers to Private. Added NonNull annotations to required fields. Added @Getters to all config classes. Clean up.

--- java/client/src/main/java/babushka/api/config/RedisClientConfiguration.java
+++ java/client/src/main/java/babushka/api/config/RedisClientConfiguration.java
@@ -1,13 +1,15 @@
 package babushka.api.config;
 
+import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 /** Represents the configuration settings for a Standalone Redis client. */
+@Getter
 @SuperBuilder
 public class RedisClientConfiguration extends BaseClientConfiguration {
   /** Strategy used to determine how and when to reconnect, in case of connection failures. */
-  public BackoffStrategy reconnectStrategy;
+  private final BackoffStrategy reconnectStrategy;
 
   /** Index of the logical database to connect to. */
-  public Integer databaseId;
+  private final Integer databaseId;
 }
