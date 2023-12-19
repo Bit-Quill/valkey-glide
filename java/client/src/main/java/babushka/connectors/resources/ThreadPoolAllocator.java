3c30797619bbc1331b691eb56ba87c1cbbb514c6
Yury-Fridlyand <yury.fridlyand@improving.com>
2023-12-18 17:56:11 -0800

    Address PR feedback.
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- java/client/src/main/java/babushka/connectors/resources/ThreadPoolAllocator.java
+++ java/client/src/main/java/babushka/connectors/resources/ThreadPoolAllocator.java
@@ -27,12 +27,12 @@ public class ThreadPoolAllocator {
   public static EventLoopGroup createNettyThreadPool(String prefix, Optional<Integer> threadLimit) {
     int threadCount = threadLimit.orElse(Runtime.getRuntime().availableProcessors());
     if (Platform.getCapabilities().isKQueueAvailable()) {
-      var name = prefix + "-kqueue-elg";
+      String name = prefix + "-kqueue-elg";
       return getOrCreate(
           name + threadCount,
           () -> new KQueueEventLoopGroup(threadCount, new DefaultThreadFactory(name, true)));
     } else if (Platform.getCapabilities().isEPollAvailable()) {
-      var name = prefix + "-epoll-elg";
+      String name = prefix + "-epoll-elg";
       return getOrCreate(
           name + threadCount,
           () -> new EpollEventLoopGroup(threadCount, new DefaultThreadFactory(name, true)));
