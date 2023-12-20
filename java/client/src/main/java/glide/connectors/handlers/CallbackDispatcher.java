02af4788fdfa7b3d34239068ca77c5eab0898f8e
Andrew Carbonetto <andrewc@bitquilltech.com>
2023-12-20 13:24:50 -0800

    Revert "Split `ClientState` and `Platform`."
    This reverts commit 0132b5a470b71efbf82ac7d98337fda1ecdfa7ae.

--- java/client/src/main/java/glide/connectors/handlers/CallbackDispatcher.java
+++ java/client/src/main/java/glide/connectors/handlers/CallbackDispatcher.java
@@ -1,6 +1,6 @@
 package glide.connectors.handlers;
 
-import babushka.connectors.ClientState;
+import babushka.managers.ClientState;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
