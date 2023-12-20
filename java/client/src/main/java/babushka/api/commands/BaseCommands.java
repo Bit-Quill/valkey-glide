1afc183054329a3d630799a27ac4c190c6ab0881
Andrew Carbonetto <andrewc@bitquilltech.com>
2023-12-20 12:20:22 -0800

    Integrate API layer with the commandManager
    Signed-off-by: Andrew Carbonetto <andrewc@bitquilltech.com>

--- java/client/src/main/java/babushka/api/commands/BaseCommands.java
+++ java/client/src/main/java/babushka/api/commands/BaseCommands.java
@@ -1,6 +1,6 @@
 package babushka.api.commands;
 
-import static babushka.ffi.resolvers.BabushkaCoreNativeDefinitions.valueFromPointer;
+import static babushka.ffi.resolvers.RedisValueResolver.valueFromPointer;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
@@ -45,6 +45,7 @@ public interface BaseCommands<T> {
   CompletableFuture<?> exec(Transaction transaction);
 
   public enum RequestType {
+    CUSTOMCOMMAND,
     GETSTRING,
     SETSTRING
   }
