8d423b7b403e94abdb97539be0a9ae5caa45b9de
Andrew Carbonetto <andrewc@bitquilltech.com>
2024-01-02 15:25:34 -0800

    Update JavaDocs and comments
    Signed-off-by: Andrew Carbonetto <andrewc@bitquilltech.com>

--- java/client/src/main/java/glide/connectors/handlers/ChannelHandler.java
+++ java/client/src/main/java/glide/connectors/handlers/ChannelHandler.java
@@ -5,19 +5,15 @@ import glide.connectors.resources.Platform;
 import glide.connectors.resources.ThreadPoolAllocator;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
 import io.netty.channel.unix.DomainSocketAddress;
-import io.netty.channel.unix.DomainSocketChannel;
-import io.netty.channel.unix.UnixChannel;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import redis_request.RedisRequestOuterClass.RedisRequest;
 import response.ResponseOuterClass.Response;
 
 /**
- * Class responsible for handling calls to/from a netty.io {@link Channel}. Uses a {@link
- * CallbackDispatcher} to record callbacks of every request sent.
+ * Class responsible for handling calls to/from a netty.io {@link Channel}.
+ * Uses a {@link CallbackDispatcher} to record callbacks of every request sent.
  */
 public class ChannelHandler {
 
@@ -28,35 +24,15 @@ public class ChannelHandler {
 
   /** Open a new channel for a new client. */
   public ChannelHandler(CallbackDispatcher callbackDispatcher, String socketPath) {
-    this(
-        ThreadPoolAllocator.createOrGetNettyThreadPool(THREAD_POOL_NAME, Optional.empty()),
-        Platform.getClientUdsNettyChannelType(),
-        new ProtobufSocketChannelInitializer(callbackDispatcher),
-        new DomainSocketAddress(socketPath),
-        callbackDispatcher);
-  }
-
-  /**
-   * Open a new channel for a new client and running it on the provided EventLoopGroup
-   *
-   * @param eventLoopGroup - ELG to run handler on
-   * @param domainSocketChannelClass - socket channel class for Handler
-   * @param channelInitializer - UnixChannel initializer
-   * @param domainSocketAddress - address to connect
-   * @param callbackDispatcher - dispatcher to handle callbacks
-   */
-  public ChannelHandler(
-      EventLoopGroup eventLoopGroup,
-      Class<? extends DomainSocketChannel> domainSocketChannelClass,
-      ChannelInitializer<UnixChannel> channelInitializer,
-      DomainSocketAddress domainSocketAddress,
-      CallbackDispatcher callbackDispatcher) {
+    // TODO: add the ability to pass in group and channel from user
     channel =
         new Bootstrap()
-            .group(eventLoopGroup)
-            .channel(domainSocketChannelClass)
-            .handler(channelInitializer)
-            .connect(domainSocketAddress)
+            // TODO let user specify the thread pool or pool size as an option
+            .group(
+                ThreadPoolAllocator.createOrGetNettyThreadPool(THREAD_POOL_NAME, Optional.empty()))
+            .channel(Platform.getClientUdsNettyChannelType())
+            .handler(new ProtobufSocketChannelInitializer(callbackDispatcher))
+            .connect(new DomainSocketAddress(socketPath))
             // TODO call here .sync() if needed or remove this comment
             .channel();
     this.callbackDispatcher = callbackDispatcher;
