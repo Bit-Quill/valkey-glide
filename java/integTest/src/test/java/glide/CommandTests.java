8896eb896b9b282419b1f98aca5f92353d33ea29
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-01-25 15:47:20 -0800

    Address PR feedback.
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- java/integTest/src/test/java/glide/CommandTests.java
+++ java/integTest/src/test/java/glide/CommandTests.java
@@ -5,20 +5,34 @@ import glide.api.models.configuration.NodeAddress;
 import glide.api.models.configuration.RedisClientConfiguration;
 import java.util.concurrent.TimeUnit;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class CommandTests {
 
+    private static RedisClient regularClient = null;
+
+    @BeforeAll
+    @SneakyThrows
+    public static void init() {
+        regularClient =
+            RedisClient.CreateClient(
+                    RedisClientConfiguration.builder()
+                        .address(NodeAddress.builder().port(TestConfiguration.STANDALONE_PORT).build())
+                        .build())
+                .get(10, TimeUnit.SECONDS);
+    }
+
+    @AfterAll
+    @SneakyThrows
+    public static void deinit() {
+        regularClient.close();
+    }
+
     @Test
     @SneakyThrows
     public void custom_command_info() {
-        var regularClient =
-                RedisClient.CreateClient(
-                                RedisClientConfiguration.builder()
-                                        .address(NodeAddress.builder().port(TestConfiguration.STANDALONE_PORT).build())
-                                        .build())
-                        .get(10, TimeUnit.SECONDS);
-        regularClient.customCommand(new String[] {"info"}).get();
-        regularClient.close();
+        regularClient.customCommand(new String[] {"info"}).get(10, TimeUnit.SECONDS);
     }
 }
