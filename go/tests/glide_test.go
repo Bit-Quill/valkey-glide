78e463b67f708e7abebd05a329fffc365787915f
aaron-congo <aaron.congo@improving.com>
2024-02-09 11:49:00 -0800

    Minor changes

--- go/tests/glide_test.go
+++ go/tests/glide_test.go
@@ -4,7 +4,8 @@ import (
 	"testing"
 )
 
-func TestNothing(t *testing.T) {
+// TODO: Replace this test with real tests when glide client implementation is started
+func TestArbitraryLogic(t *testing.T) {
 	someVar := true
 	if !someVar {
 		t.Fatalf("Expected someVar to be true, but was false.")
