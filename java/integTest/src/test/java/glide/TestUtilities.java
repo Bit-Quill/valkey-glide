45b7afdd7db6bf55ffb3d7aa40ac95da11547123
aaron-congo <aaron.congo@improving.com>
2024-02-14 09:44:44 -0800

    Add sadd and srem to transaction test

--- java/integTest/src/test/java/glide/TestUtilities.java
+++ java/integTest/src/test/java/glide/TestUtilities.java
@@ -6,19 +6,23 @@ import glide.api.models.commands.SetOptions;
 import java.util.UUID;
 
 public class TestUtilities {
+    private static final String key1 = "{key}" + UUID.randomUUID();
+    private static final String key2 = "{key}" + UUID.randomUUID();
+    private static final String value1 = "{value}" + UUID.randomUUID();
+    private static final String value2 = "{value}" + UUID.randomUUID();
 
     public static BaseTransaction transactionTest(BaseTransaction baseTransaction) {
-        String key1 = "{key}" + UUID.randomUUID();
-        String key2 = "{key}" + UUID.randomUUID();
-
-        baseTransaction.set(key1, "bar");
-        baseTransaction.set(key2, "baz", SetOptions.builder().returnOldValue(true).build());
+        baseTransaction.set(key1, value1);
+        baseTransaction.set(key2, value2, SetOptions.builder().returnOldValue(true).build());
         baseTransaction.customCommand("MGET", key1, key2);
+
+        baseTransaction.sadd(key1, new String[]{value1, value2});
+        baseTransaction.srem(key1, new String[]{value1, value2});
 
         return baseTransaction;
     }
 
     public static Object[] transactionTestResult() {
-        return new Object[] {"OK", null, new String[] {"bar", "baz"}};
+        return new Object[] {"OK", null, new String[] {"bar", "baz"}, 2, 2};
     }
 }
