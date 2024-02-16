c8ad03585a88b42e6c25862bacc2f9df3e1e0c80
Jonathan Louie <jonathanl@bitquilltech.com>
2024-02-16 13:55:10 -0800

    Add integration test for transactions

--- java/integTest/src/test/java/glide/TestUtilities.java
+++ java/integTest/src/test/java/glide/TestUtilities.java
@@ -3,6 +3,8 @@ package glide;
 
 import glide.api.models.BaseTransaction;
 import glide.api.models.commands.SetOptions;
+import glide.api.models.commands.ZaddOptions;
+import java.util.Map;
 import java.util.UUID;
 
 public class TestUtilities {
@@ -10,15 +12,22 @@ public class TestUtilities {
     public static BaseTransaction transactionTest(BaseTransaction baseTransaction) {
         String key1 = "{key}" + UUID.randomUUID();
         String key2 = "{key}" + UUID.randomUUID();
+        String keySortedSet = "{key}" + UUID.randomUUID();
 
         baseTransaction.set(key1, "bar");
         baseTransaction.set(key2, "baz", SetOptions.builder().returnOldValue(true).build());
         baseTransaction.customCommand("MGET", key1, key2);
 
+        Map<String, Double> membersScores = Map.of("baz", 1.0, "foo", 2.0);
+        baseTransaction.zadd(keySortedSet, membersScores, ZaddOptions.builder().build(), false);
+        baseTransaction.zaddIncr(keySortedSet, "baz", 2.0d, ZaddOptions.builder().build());
+        baseTransaction.zrem(keySortedSet, new String[] {"foo"});
+        baseTransaction.zcard(keySortedSet);
+
         return baseTransaction;
     }
 
     public static Object[] transactionTestResult() {
-        return new Object[] {"OK", null, new String[] {"bar", "baz"}};
+        return new Object[] {"OK", null, new String[] {"bar", "baz"}, 2L, 3.0d, 1L, 1L};
     }
 }
