ec1332c14ba0b0720ae3aa8450b51aec8257cb28
jonathanl-bq <72158117+jonathanl-bq@users.noreply.github.com>
2024-02-16 18:19:04 -0800

    Update java/client/src/main/java/glide/api/commands/SortedSetCommands.java
    Co-authored-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- java/client/src/main/java/glide/api/commands/SortedSetCommands.java
+++ java/client/src/main/java/glide/api/commands/SortedSetCommands.java
@@ -68,7 +68,7 @@ public interface SortedSetCommands {
      * @param key - The key of the sorted set.
      * @param members - A list of members to remove from the sorted set.
      * @returns The number of members that were removed from the sorted set, not including
-     *     non-existing members. If `key` does not exist, it is treated as an empty sorted set, and
+     *     non-existing members. If <code>key</code> does not exist, it is treated as an empty sorted set, and
      *     this command returns 0. If `key` holds a value that is not a sorted set, an error is
      *     returned.
      */
