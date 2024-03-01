6ad2d24fc2e1e34ecd80a14e923c3a1180046582
aaron-congo <aaron.congo@improving.com>
2024-03-01 11:08:29 -0800

    Add Rust logic

---
+++ go/examples/main.go
@@ -0,0 +1,34 @@
+// Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0
+
+package examples
+
+import (
+	"fmt"
+	"github.com/aws/glide-for-redis/go/glide/api"
+)
+
+func main() {
+	fmt.Println("Starting go-glide client...")
+	config := api.NewRedisClientConfiguration()
+
+	client, err := api.CreateClient(config)
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.Set("FOO", "BAR")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("SET FOO : BAR")
+
+	val, err := client.Get("FOO")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("GET FOO :", val)
+
+	client.Close()
+
+	fmt.Println("Disconnected from Redis")
+}
