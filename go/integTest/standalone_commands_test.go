7516d97146bb7b5c7041d39d6eff42711c6e4a4f
aaron-congo <aaron.congo@improving.com>
2024-03-14 14:55:30 -0700

    Move error handling tests to standalone command tests

--- go/integTest/standalone_commands_test.go
+++ go/integTest/standalone_commands_test.go
@@ -2,7 +2,10 @@
 
 package integTest
 
-import "github.com/stretchr/testify/assert"
+import (
+	"github.com/aws/glide-for-redis/go/glide/api"
+	"github.com/stretchr/testify/assert"
+)
 
 func (suite *GlideTestSuite) TestCustomCommand() {
 	client := suite.createClient()
@@ -12,5 +15,29 @@ func (suite *GlideTestSuite) TestCustomCommand() {
 	assert.Nil(suite.T(), err)
 	assert.Equal(suite.T(), "PONG", result)
 
-	_ = client.Close()
+	client.Close()
+}
+
+func (suite *GlideTestSuite) TestInvalidCustomCommand() {
+	client := suite.createClient()
+
+	result, err := client.CustomCommand([]string{"pewpew"})
+
+	assert.Nil(suite.T(), result)
+	assert.NotNil(suite.T(), err)
+	assert.IsType(suite.T(), &api.RequestError{}, err)
+
+	client.Close()
+}
+
+func (suite *GlideTestSuite) TestInvalidCustomCommandArgs() {
+	client := suite.createClient()
+
+	result, err := client.CustomCommand([]string{"ping", "pang", "pong"})
+
+	assert.Nil(suite.T(), result)
+	assert.NotNil(suite.T(), err)
+	assert.IsType(suite.T(), &api.RequestError{}, err)
+
+	client.Close()
 }
