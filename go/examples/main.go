2b63825290c1347e83dcb332e33989bca60cc82f
aaron-congo <aaron.congo@improving.com>
2024-03-14 13:12:33 -0700

    Fix build, address PR comments

--- go/examples/main.go
+++ go/examples/main.go
@@ -1,23 +1,24 @@
 // Copyright GLIDE-for-Redis Project Contributors - SPDX Identifier: Apache-2.0
 
-package examples
+package main
 
 import (
-    "github.com/aws/glide-for-redis/go/glide/api"
-    "log"
+	"log"
+
+	"github.com/aws/glide-for-redis/go/glide/api"
 )
 
 func main() {
-    host := "localhost"
-    port := 6379
+	host := "localhost"
+	port := 6379
 
-    config := api.NewRedisClientConfiguration().
-        WithAddress(&api.NodeAddress{host, port})
+	config := api.NewRedisClientConfiguration().
+		WithAddress(&api.NodeAddress{Host: host, Port: port})
 
-    client, err := api.CreateClient(config)
-    if err != nil {
-        log.Fatal("error connecting to database: ", err)
-    }
+	client, err := api.CreateClient(config)
+	if err != nil {
+		log.Fatal("error connecting to database: ", err)
+	}
 
-    client.Close()
+	client.Close()
 }
