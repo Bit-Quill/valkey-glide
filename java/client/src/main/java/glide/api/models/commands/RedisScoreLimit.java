c8be4c580d0d44517b7078a8f4402fd807e112fe
SanHalacogluImproving <san.halacoglu@improving.com>
2024-03-19 16:12:00 -0700

    Added documentation to RedisScoreLimit.

--- java/client/src/main/java/glide/api/models/commands/RedisScoreLimit.java
+++ java/client/src/main/java/glide/api/models/commands/RedisScoreLimit.java
@@ -6,6 +6,7 @@ public class RedisScoreLimit {
         String toArg();
     }
 
+    /** Enumeration representing numeric positive and negative infinity bounds for a sorted set. */
     public enum InfBound implements ScoreLimit {
         POSITIVE_INFINITY("+inf"),
         NEGATIVE_INFINITY("-inf");
@@ -22,13 +23,22 @@ public class RedisScoreLimit {
         }
     }
 
+    /** Represents a specific numeric score boundary in a sorted set. */
     public static class ScoreBoundary implements ScoreLimit {
+        /** The score value. */
         private final double bound;
+
+        /** Whether the score value is inclusive. */
         private final boolean isInclusive;
 
         public ScoreBoundary(double bound, boolean isInclusive) {
             this.bound = bound;
             this.isInclusive = isInclusive;
+        }
+
+        public ScoreBoundary(double bound) {
+            this.bound = bound;
+            this.isInclusive = true;
         }
 
         @Override
