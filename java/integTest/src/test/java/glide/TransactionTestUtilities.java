cdc80e11193388303ecc15aeaf88bb66ff4217b6
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-03-27 11:30:55 -0700

    Copy from #165.
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- java/integTest/src/test/java/glide/TransactionTestUtilities.java
+++ java/integTest/src/test/java/glide/TransactionTestUtilities.java
@@ -214,6 +214,56 @@ public class TransactionTestUtilities {
             OK, // pfmerge(hllKey3, new String[] {hllKey1, hllKey2})
             3L, // pfcount(new String[] { hllKey3 })
 
+/*
+            OK, // set(key1, value1);
+            value1, // get(key1);
+            null, // set(key2, value2, returnOldValue(true));
+            new String[] {value1, value2}, // customCommand(new String[] {"MGET", key1, key2});
+            1L, // exists(new String[] {key1});
+            1L, // del(new String[] {key1});
+            null, // get(key1);
+            1L, // unlink(new String[] {key2});
+            null, // get(key2);
+            OK, // mset(Map.of(key1, value2, key2, value1));
+            new String[] {value2, value1}, // mget(new String[] {key1, key2});
+            1L, // incr(key3);
+            3L, // incrBy(key3, 2);
+            2L, // decr(key3);
+            0L, // decrBy(key3, 2);
+            0.5, // incrByFloat(key3, 0.5);
+            1L, // unlink(new String[] {key3});
+            2L, // hset(key4, Map.of(field1, value1, field2, value2));
+            value1, // hget(key4, field1);
+            true, // hexists(key4, field2);
+            new String[] {
+                value1, null, value2
+            }, // hmget(key4, new String[] {field1, "non_existing_field", field2});
+            Map.of(field1, value1, field2, value2), // hgetall(key4);
+            1L, // hdel(key4, new String[] {field1});
+            5L, // hincrBy(key4, field3, 5);
+            10.5, // hincrByFloat(key4, field3, 5.5);
+            5L, // lpush(key5, new String[] {value1, value1, value2, value3, value3});
+            5L, // llen(key5);
+            1L, // lrem(key5, 1, value1);
+            OK, // ltrim(key5, 1, -1);
+            new String[] {value3, value2}, // lrange(key5, 0, -2);
+            value3, // lpop(key5);
+            new String[] {value2, value1}, // lpopCount(key5, 2);
+            3L, // rpush(key6, new String[] {value1, value2, value2});
+            value2, // rpop(key6);
+            new String[] {value2, value1}, // rpopCount(key6, 2);
+            2L, // sadd(key7, new String[] {"baz", "foo"});
+            1L, // srem(key7, new String[] {"foo"});
+            1L, // scard(key7);
+            Set.of("baz"), // smembers(key7);
+            3L, // zadd(key8, Map.of("one", 1.0, "two", 2.0, "three", 3.0));
+            4.0, // zaddIncr(key8, "one", 3);
+            1L, // zrem(key8, new String[] {"one"});
+            2L, // zcard(key8);
+            OK, // configSet(Map.of("timeout", "1000"));
+            Map.of("timeout", "1000"), // configGet(new String[] {"timeout"});
+            OK // configResetStat();
+*/
         };
     }
 }
