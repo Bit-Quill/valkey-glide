094978158e2f00a2eb7da96f12b38cfb1a2aec71
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-04-11 14:39:08 -0700

    Update docs.
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- python/python/glide/async_commands/core.py
+++ python/python/glide/async_commands/core.py
@@ -787,7 +787,7 @@ class CoreCommands(Protocol):
 
     async def lpushx(self, key: str, elements: List[str]) -> int:
         """
-        Inserts specified values at the head of the `list`, only if `key` already exists and holds a list.
+        Inserts all the specified values at the head of the list stored at `key`, only if the list already exists.
 
         See https://redis.io/commands/lpushx/ for more details.
 
@@ -949,7 +949,7 @@ class CoreCommands(Protocol):
 
     async def rpushx(self, key: str, elements: List[str]) -> int:
         """
-        Inserts specified values at the tail of the `list`, only if `key` already exists and holds a list.
+        Inserts all the specified values at the tail of the list stored at `key`, only if the list already exists.
 
         See https://redis.io/commands/rpushx/ for more details.
 
