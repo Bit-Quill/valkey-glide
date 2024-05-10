216a4847c8e7aacf59045296766aa3b24d138559
TJ Zhang <tj.zhang@improving.com>
2024-05-09 19:11:03 -0700

    undo changes to examplesApp

--- java/examples/src/main/java/glide/examples/ExamplesApp.java
+++ java/examples/src/main/java/glide/examples/ExamplesApp.java
@@ -2,10 +2,8 @@
 package glide.examples;
 
 import glide.api.RedisClient;
-import glide.api.models.commands.geospatial.GeospatialData;
 import glide.api.models.configuration.NodeAddress;
 import glide.api.models.configuration.RedisClientConfiguration;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class ExamplesApp {
@@ -21,10 +19,10 @@ public class ExamplesApp {
         boolean useSsl = false;
 
         RedisClientConfiguration config =
-                RedisClientConfiguration.builder()
-                        .address(NodeAddress.builder().host(host).port(port).build())
-                        .useTLS(useSsl)
-                        .build();
+            RedisClientConfiguration.builder()
+                .address(NodeAddress.builder().host(host).port(port).build())
+                .useTLS(useSsl)
+                .build();
 
         try {
             RedisClient client = RedisClient.CreateClient(config).get();
@@ -34,21 +32,6 @@ public class ExamplesApp {
 
             System.out.println("SET(apples, oranges): " + client.set("apples", "oranges").get());
             System.out.println("GET(apples): " + client.get("apples").get());
-
-            // When added via GEOADD, the geospatial coordinates are converted into a 52 bit geohash, so
-            // the coordinates
-            // returned might not be exactly the same as the input values
-            client
-                    .geoadd(
-                            "mySortedSet",
-                            Map.of(
-                                    "Palermo",
-                                    new GeospatialData(13.361389, 38.115556),
-                                    "Catania",
-                                    new GeospatialData(15.087269, 37.502669)))
-                    .get();
-            Double result = client.geodist("mySortedSet", "Palermo", "Catania").get();
-            System.out.println(result);
 
         } catch (ExecutionException | InterruptedException e) {
             System.out.println("Glide example failed with an exception: ");
