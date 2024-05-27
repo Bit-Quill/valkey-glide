bbf44f1f19ad46f0b97f9678f89ed7dfa29dbe48
aaron-congo <aaron.congo@improving.com>
2024-05-27 13:37:05 -0700

    Python: add OBJECT FREQ command

--- python/python/tests/test_async_client.py
+++ python/python/tests/test_async_client.py
@@ -3320,6 +3320,24 @@ class TestCommands:
         with pytest.raises(RequestError):
             await redis_client.pfadd("foo", [])
 
+    @pytest.mark.parametrize("cluster_mode", [True, False])
+    @pytest.mark.parametrize("protocol", [ProtocolVersion.RESP2, ProtocolVersion.RESP3])
+    async def test_object_freq(self, redis_client: TRedisClient):
+        key = get_random_string(10)
+        maxmemory_policy_key = "maxmemory-policy"
+        config = await redis_client.config_get([maxmemory_policy_key])
+        maxmemory_policy = cast(str, config.get(maxmemory_policy_key))
+
+        try:
+            assert (
+                await redis_client.config_set({maxmemory_policy_key: "allkeys-lfu"}) == OK
+            )
+            assert await redis_client.object_freq("non_existing_key") is None
+            assert await redis_client.set(key, "") == OK
+            assert await redis_client.object_freq(key) >= 0
+        finally:
+            await redis_client.config_set({maxmemory_policy_key: maxmemory_policy})
+
 
 class TestMultiKeyCommandCrossSlot:
     @pytest.mark.parametrize("cluster_mode", [True])
