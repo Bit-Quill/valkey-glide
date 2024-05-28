cfeda0c6e4e00b01cf30400bdabb0c12ad8f4656
Yury-Fridlyand <yury.fridlyand@improving.com>
2024-05-28 12:41:24 -0700

    Apply suggestions from code review
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    Co-authored-by: Aaron <69273634+aaron-congo@users.noreply.github.com>

--- java/client/src/main/java/glide/api/commands/ScriptingAndFunctionsClusterCommands.java
+++ java/client/src/main/java/glide/api/commands/ScriptingAndFunctionsClusterCommands.java
@@ -13,13 +13,13 @@ import java.util.concurrent.CompletableFuture;
 public interface ScriptingAndFunctionsClusterCommands {
 
     /**
-     * Loads a library to Redis unless a library with the same name exists.<br>
+     * Loads a library to Redis.<br>
      * The command will be routed to all primary nodes.
      *
      * @since Redis 7.0 and above.
      * @see <a href="https://redis.io/docs/latest/commands/function-load/">redis.io</a> for details.
      * @param libraryCode The source code that implements the library.
-     * @param replace Whether new library overwrites a library with the same name if it exists.
+     * @param replace Whether the given library should overwrite a library with the same name if it already exists.
      * @return The library name that was loaded.
      * @example
      *     <pre>{@code
