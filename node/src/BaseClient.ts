ff4b12d177f76af875da86c6ea030de590a9a0e5
Yi-Pin Chen <yi-pin.chen@improving.com>
2024-06-06 17:03:10 -0700

    Addressed review comment

--- node/src/BaseClient.ts
+++ node/src/BaseClient.ts
@@ -2386,6 +2386,7 @@ export class BaseClient {
      *
      * See https://valkey.io/commands/pfcount/ for more details.
      *
+     * @remarks When in cluster mode, all `keys` must map to the same hash slot.
      * @param keys - The keys of the HyperLogLog data structures to be analyzed.
      * @returns - The approximated cardinality of given HyperLogLog data structures.
      *     The cardinality of a key that does not exist is 0.
