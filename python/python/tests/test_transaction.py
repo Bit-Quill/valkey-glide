e4eb6c2f304f9d3b8838df545042366b3331b80f
Yi-Pin Chen <yi-pin.chen@improving.com>
2024-06-21 15:02:53 -0700

    Fixed CI/CD test error

--- python/python/tests/test_transaction.py
+++ python/python/tests/test_transaction.py
@@ -655,31 +655,33 @@ class TestTransaction:
         assert result[5:12] == [2, 2, 2, ["Bob", "Alice"], 2, OK, None]
         assert result[12:] == expected
 
-    @pytest.mark.parametrize("cluster_mode", [False])
-    @pytest.mark.parametrize("protocol", [ProtocolVersion.RESP2, ProtocolVersion.RESP3])
-    async def test_standalone_copy_transaction(self, redis_client: RedisClient):
-        min_version = "6.2.0"
-        if await check_if_server_version_lt(redis_client, min_version):
-            return pytest.mark.skip(reason=f"Redis version required >= {min_version}")
-
-        keyslot = get_random_string(3)
-        key = "{{{}}}:{}".format(keyslot, get_random_string(3))  # to get the same slot
-        key1 = "{{{}}}:{}".format(keyslot, get_random_string(3))  # to get the same slot
-        value = get_random_string(5)
-        transaction = Transaction()
-        transaction.select(1)
-        transaction.set(key, value)
-        transaction.copy(key, key1, 1, replace=True)
-        transaction.get(key1)
-        result = await redis_client.exec(transaction)
-        assert result[3] == value
-
     def test_transaction_clear(self):
         transaction = Transaction()
         transaction.info()
         transaction.select(1)
         transaction.clear()
         assert len(transaction.commands) == 0
+
+    @pytest.mark.parametrize("cluster_mode", [False])
+    @pytest.mark.parametrize("protocol", [ProtocolVersion.RESP2, ProtocolVersion.RESP3])
+    async def test_standalone_copy_transaction(self, redis_client: RedisClient):
+        min_version = "6.2.0"
+        if not await check_if_server_version_lt(redis_client, min_version):
+            keyslot = get_random_string(3)
+            key = "{{{}}}:{}".format(
+                keyslot, get_random_string(3)
+            )  # to get the same slot
+            key1 = "{{{}}}:{}".format(
+                keyslot, get_random_string(3)
+            )  # to get the same slot
+            value = get_random_string(5)
+            transaction = Transaction()
+            transaction.select(1)
+            transaction.set(key, value)
+            transaction.copy(key, key1, 1, replace=True)
+            transaction.get(key1)
+            result = await redis_client.exec(transaction)
+            assert result[3] == value
 
     @pytest.mark.parametrize("cluster_mode", [True, False])
     @pytest.mark.parametrize("protocol", [ProtocolVersion.RESP2, ProtocolVersion.RESP3])
